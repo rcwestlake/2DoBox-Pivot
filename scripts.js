3f273ec4cb31aa5f2d3a2ad8ab760eda597b82e6
Kristen Burgess <kristen.burgess@doane.edu>
2016-09-08 14:11:56 -0600

    add style to ul

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
 var saveButton = $('.savebtn');
 var searchField = $('#search');
+var title = $('#title').val();
+var body = $('#body').val();
 
 function saveIdea (title, body) {
   return $('.ideaList').append(`
     <li class="idea">
-    <span> ${$('.title').val()} </span>
-    <span> ${$('.body').val()} </span> </li>
+    <span class="title"> ${$('#title').val()} </span>
+    <span class="body"> ${$('#body').val()} </span> </li>
     `);
   }
 
 $('.savebtn').on('click', function (event) {
   event.preventDefault()
-  var title = $('.title').val();
-  var body = $('.body').val();
-
   saveIdea(title, body);
-
 });
+
+
 
 
 
