462ae549ff8e57c962218644ddfd444c88c0f043
Kristen Burgess <kristen.burgess@doane.edu>
2016-09-11 12:52:55 -0600

    change way inputs are fed to storage

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
-var saveButton = $('.savebtn');
-var searchField = $('.search-field');
-var titleInput = $('.title-input');
-var bodyInput = $('.body-input');
+var $titleInput = $('.title-input');
+var $bodyInput = $('.body-input');
+var $saveButton = $('.savebtn');
 
 //onLoad Functions, retrieve local storage//
 
@@ -9,7 +8,7 @@ onLoad();
 
 function onLoad(){
   checkLocalMakeLocal();
-  displayIdeas();
+  populateDOM();
 };
 
 function checkLocalMakeLocal(){
@@ -18,36 +17,36 @@ function checkLocalMakeLocal(){
   };
 };
 
-function getIdeas(){
- return JSON.parse(localStorage.getItem("ideas"));
-};
-
-function displayIdeas(){
+function populateDOM(){
   var ideas = getIdeas();
   ideas.forEach(function(idea) {
     addNewIdea(idea.id, idea.title, idea.body, idea.quality);
   });
 };
 
+function getIdeas(){
+ return JSON.parse(localStorage.getItem("ideas"));
+};
+
+
 //defines an idea//
-function Idea(id, title, body, quality){
-  this.id = Date.now();
+function Idea(id, title, body, quality) {
+  this.id = parseInt(id);
   this.title = title;
   this.body = body;
   this.quality = quality;
 };
 
-//generates random id for each idea //
-// function idGenerator() {
-//   return Date.now().tostring();
-// };
+// generates random id for each idea //
+function idGenerator() {
+  return Date.now().toString();
+};
 
 //save button disabled if input fields are empty//
 
 function toggleButton (){
   if ($('.title-input').val().length > 0  || $('.body-input').val().length > 0) {
     $('.savebtn').attr('disabled', false);
-
   } else {
     $('.savebtn').attr('disabled', true);
   }
@@ -58,37 +57,34 @@ $('.body-input').on('keyup', function(){
 
 //grabs what user types in title & body input fields//
 function getTitle() {
-  var ideaTitle = titleInput.val();
+  var ideaTitle = $titleInput.val();
   return ideaTitle;
 };
 
 function getBody() {
-  var ideaBody = bodyInput.val();
+  var ideaBody = $bodyInput.val();
   return ideaBody;
 };
 
 function clearInputs() {
-  $('#form')[0].reset();
-}
+  $titleInput.val('');
+  $bodyInput.val('');
+};
 
 //takes inputs, puts them into an object for storage, pushes that box to storage, and displays new box on screen//
-function addNewIdea() {
-  var newIdea = new Idea(Date.now(), getTitle(), getBody(), 'Swill')
+function makeNewIdea() {
+  var newIdea = new Idea(idGenerator(), getTitle(), getBody(), 'Swill')
   currentIdeas= getIdeas();
   currentIdeas.push(newIdea);
   localStorage.setItem('ideas', JSON.stringify(currentIdeas));
   formIdea(newIdea.id, newIdea.title, newIdea.body, 'Swill');
-};
-
-$('.savebtn').on('click', function (event) {
-  event.preventDefault();
-  addNewIdea();
   clearInputs();
   toggleButton();
-});
+};
 
+$($saveButton).on('click', makeNewIdea);
 
-function formIdea (id, title, body, quality) {
+function makeIdeaCard (id, title, body, quality) {
   return $('.ideaList').prepend(`
     <article id="`+ id +`" class="idea-card">
     <h2> class="editable" contenteditable="true">` + title + `</h2>
@@ -98,7 +94,7 @@ function formIdea (id, title, body, quality) {
     <button class="down"></button>
     <p class="idea-quality ` + quality +`"><span>Quality:</span> <span class="quality-displayed">` + quality + `</span> </p>
     <hr>
-    </li>
+    </article>
     `);
   };
 
