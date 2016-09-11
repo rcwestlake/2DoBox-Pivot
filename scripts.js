824375932f737bffdd02cfb4041ed782071111f7
Kristen Burgess <kristen.burgess@doane.edu>
2016-09-11 10:17:40 -0600

    js changes, adding constructors and redoing so much

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 var saveButton = $('.savebtn');
 var searchField = $('#search');
-var title = $('#title').val();
-var body = $('#body').val();
+var titleInput = $('#title');
+var bodyInput = $('#body');
 var ideaBox = [];
-// var quality = $('.qualityword');
+// var quality = '.quality';
 
 // var storage = getLocalStorage() || [];
 // renderToPage();
@@ -11,21 +11,53 @@ var ideaBox = [];
 // voteUp();
 
 
-function Idea(title, body, id, quality){
-  this.title = title;
-  this.body = body;
-  this.id = Date.now();
-  // this.quality = $('.qualityword');
-}
 
 function storeIdea(title){
  this.title= title;
  localStorage.setItem('Idea', JSON.stringify(ideaBox));
 }
 
-function getIdea(title){
- var retrieveIdea = JSON.parse(localStorage.getItem(Idea));
+function getIdeas(title){
+ var retrieveIdea = JSON.parse(localStorage.getItem(ideaBox));
 }
+
+//defines an idea//
+function Idea(title, body, id, quality){
+  this.title = title;
+  this.body = body;
+  this.id = parseInt(id);
+  this.quality = quality;
+};
+
+//generates random id for each idea box//
+function randomId() {
+  return Date.now().tostring();
+};
+
+//grabs what user types in title & body input fields//
+function getTitle() {
+  var ideaTitle = titleInput.val();
+  return ideaTitle;
+};
+
+function getBody() {
+  var ideaBody = bodyInput.val();
+  return ideaBody;
+};
+
+function clearInputs() {
+  $('#form')[0].reset();
+}
+
+//takes inputs, puts them into an object for storage, pushes that box to storage, and displays new box on screen//
+function addNewIdea() {
+  var newIdea = new Idea(randomID(), getTitle(), getBody(), 'Swill')
+  currentIdeas= getIdeas();
+  currentIdeas.push(newIdea);
+  local.Storage.setItem('ideaBox', JSON.stringify(currentIdeas));
+  formIdea(newIdea.id, newIdea.title, newIdea.body, 'Swill');
+  clearInputs();
+};
 
 $('.savebtn').on('click', function() {
   var title = $('#title').val();
@@ -38,13 +70,11 @@ $('.savebtn').on('click', function() {
 $('.savebtn').on('click', function (event) {
   event.preventDefault();
   formIdea(title, body);
-  $('#form')[0].reset();
-
   toggleButton();
 });
 
 
-function formIdea (title, body) {
+function formIdea (id, title, body, quality) {
   return $('.ideaList').prepend(`
     <li class="idea swill" id= "${Date.now()}">
 
