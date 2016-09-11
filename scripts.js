1863c7b6748c1a7fcf049952e041044423301c1f
Kristen Burgess <kristen.burgess@doane.edu>
2016-09-11 10:53:25 -0600

    add onload storage functions

--- scripts.js
+++ scripts.js
@@ -3,15 +3,23 @@ var searchField = $('#search');
 var titleInput = $('#title');
 var bodyInput = $('#body');
 var ideaBox = [];
-// var quality = '.quality';
 
-// var storage = getLocalStorage() || [];
-// renderToPage();
-// voteDown();
-// voteUp();
+//onLoadFunctions//
 
+onLoad();
 
+function onLoad(){
+  checkLocalMakeLocal();
+  displayIdeas();
+};
 
+function checkLocalMakeLocal(){
+  if (localStorage.getItem("ideaBox") === null){
+    localStorage.setItem("ideabox", JSON.stringify([]))
+  };
+};
+
+//STORAGE//
 function storeIdea(title){
  this.title= title;
  localStorage.setItem('Idea', JSON.stringify(ideaBox));
@@ -20,6 +28,7 @@ function storeIdea(title){
 function getIdeas(title){
  var retrieveIdea = JSON.parse(localStorage.getItem(ideaBox));
 }
+
 
 //defines an idea//
 function Idea(title, body, id, quality){
@@ -33,6 +42,20 @@ function Idea(title, body, id, quality){
 function randomId() {
   return Date.now().tostring();
 };
+
+//save button disabled if input fields are empty//
+
+function toggleButton (){
+  if ($('#title').val().length > 0  || $('#body').val().length > 0) {
+    $('.savebtn').attr('disabled', false);
+
+  } else {
+    $('.savebtn').attr('disabled', true);
+  }
+}
+$('#body').on('keyup', function(){
+  toggleButton();
+});
 
 //grabs what user types in title & body input fields//
 function getTitle() {
@@ -59,107 +82,28 @@ function addNewIdea() {
   clearInputs();
 };
 
-$('.savebtn').on('click', function() {
-  var title = $('#title').val();
-  var body = $('#body').val();
-  var idea = new Idea(title, body);
-  ideaBox.push(idea);
- storeIdea(ideaBox);
-});
-
 $('.savebtn').on('click', function (event) {
   event.preventDefault();
-  formIdea(title, body);
+  addNewIdea();
   toggleButton();
 });
 
 
 function formIdea (id, title, body, quality) {
   return $('.ideaList').prepend(`
-    <li class="idea swill" id= "${Date.now()}">
-
-    <div class="title"> ${$('#title').val()} <input type="image" src="images/delete.svg" class="delete">
-    </input>
-    </div>
-
-    <div class="body"> ${$('#body').val()} </div>
-
-    <div class="quality">
-    <input type="image" src="images/downvote.svg" class="down">
-    <input type="image" src="images/upvote.svg"
-    class="up">
-    <p class="qualityword">quality: swill </p>
-    </div>
+    <li id="`+ id +`" class="idea-box">
+    <h2> class="editable" contenteditable="true">` + title + `</h2>
+    <button class="delete"></button>
+    <p class="editable idea-body" contenteditable="true">`+ body +`</p>
+    <button class="up"></button>
+    <button class="down"></button>
+    <p class="idea-quality ` + quality +`"><span>Quality:</span> <span class="quality-displayed">` + quality + `</span> </p>
     <hr>
     </li>
-
     `);
   };
 
 
-function toggleButton (){
-  if ($('#title').val().length > 0  || $('#body').val().length > 0) {
-    $('.savebtn').attr('disabled', false);
-
-  } else {
-    $('.savebtn').attr('disabled', true);
-  }
-}
-$('#body').on('keyup', function(){
-  toggleButton();
-});
-
 // $('ul').on('click','.up', function () {
 //   alert ('YO YO YO')
 // });
-
-$('ul').on('click', '.delete', function () {
-  $(this).parent().parent().remove();
-});
-
-//UpVote//
-$('ul').on('click', '.up', function () {
-  if ($(this).parent().parent().hasClass('swill')) {
-  $(this).parent().parent().addClass('plausible');
-  $(this).parent().parent().removeClass('swill'); }
- else if ($(this).parent().parent().hasClass('plausible')) {
-$(this).parent().parent().addClass('genius');
-$(this).parent().parent().removeClass('plausible');
- }});
-
- //DownVote//
-
- $('ul').on('click', '.down', function () {
-   if ($(this).parent().parent().hasClass('genius')) {
-   $(this).parent().parent().addClass('plausible');
-   $(this).parent().parent().removeClass('genius'); }
-  else if ($(this).parent().parent().hasClass('plausible')) {
- $(this).parent().parent().addClass('swill');
- $(this).parent().parent().removeClass('plausible');
-  }});
-
-// Display Quality - UpVotes //
-
-$('ul').on('click', '.up', function () {
-if ($(this).parent().parent().hasClass('swill')) {
-  $(this).siblings('.qualityword').text('quality: swill'); }
-else if
-($(this).parent().parent().hasClass('plausible')) {
-  $(this).siblings('.qualityword').text('quality: plausible'); }
-else if
-($(this).parent().parent().hasClass('genius')) {
-  $(this).siblings('.qualityword').text('quality: genius'); }
-});
-
-// Display Qulity - DownVotes //
-
-$('ul').on('click', '.down', function () {
-if ($(this).parent().parent().hasClass('swill')) {
-  $(this).siblings('.qualityword').text('quality: swill'); }
-else if
-($(this).parent().parent().hasClass('plausible')) {
-  $(this).siblings('.qualityword').text('quality: plausible'); }
-else if
-($(this).parent().parent().hasClass('genius')) {
-  $(this).siblings('.qualityword').text('quality: genius'); }
-});
